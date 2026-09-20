4f9073cd16d3170a5ec8aaea3ecaf55fb591ac78
sQuidri <sadiq_m.q@outlook.com>
2026-09-20 14:04:17 +1000

    add cloudflare pages preset and config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
+  nitro: {
+    preset: "cloudflare-pages",
+  },
+
   components: [
     //global: true,
     "~/components",
